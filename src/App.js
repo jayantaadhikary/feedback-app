9959d2ef2d604d7769366ebb352e9223e8bfa347
Jayanta Adhikary <73451350+jayantaadhikary@users.noreply.github.com>
2022-06-21 11:06:29 +0530

    adding form to the app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
+import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
 import FeedbackData from "./data/FeedbackData";
+import FeedbackForm from "./components/FeedbackForm";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
 
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
+
   const deleteFeedback = (id) => {
-    if(window.confirm('Are you sure if you want to delete?')){
-      setFeedback(feedback.filter((item)=>{
-        return item.id !== id
-      }))
+    if (window.confirm("Are you sure if you want to delete?")) {
+      setFeedback(
+        feedback.filter((item) => {
+          return item.id !== id;
+        })
+      );
     }
-    
   };
 
   return (
     <>
       <Header />
       <div className="container">
+        <FeedbackForm handleAdd={addFeedback} />
+        <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
